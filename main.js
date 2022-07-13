b0bd279cc2b3a8337b797b2ce2cb3a7d7a02e070
Franco Topatigh <topatighfranco@gmail.com>
2022-07-13 00:06:31 -0300

    actualizamos algunas camisetas

--- main.js
+++ main.js
@@ -26,17 +26,17 @@ class Shirt {
 
 // Objetos
 
-const shirt1 = new Shirt ("1", "Inter Visitante", "21/22", 8499, "Liga Italiana", "allProducts", "img/inter-shirt.jpg");
-const shirt2 = new Shirt ("2", "Milan Titular", "21/22", 8499, "Liga Italiana", "allProducts", "img/milan-shirt.jpg");
+const shirt1 = new Shirt ("1", "Inter Titular", "22/23", 8999, "Liga Italiana", "allProducts", "img/inter-shirt.png");
+const shirt2 = new Shirt ("2", "Milan Titular", "22/22", 8999, "Liga Italiana", "allProducts", "img/milan-shirt.png");
 const shirt3 = new Shirt ("3","Juventus Titular", "22/23", 8999, "Liga Italiana", "allProducts" , "img/juve-shirt.png");
 const shirt4 = new Shirt ("4", "Manchester City", "22/23", 9699,"Liga Inglesa", "allProducts" , "img/city-shirt.png");
-const shirt5 = new Shirt ("5", "Manchester United", "21/22", 8499, "Liga Inglesa", "allProducts" , "img/manunited-shirt.png");
+const shirt5 = new Shirt ("5", "Manchester United", "22/23", 9699, "Liga Inglesa", "allProducts" , "img/manchester-shirt.png");
 const shirt6 = new Shirt ("6", "Real Madrid Titular", "22/23", 9999, "Liga Española", "allProducts", "img/madrid-shirt.webp");
 const shirt7 = new Shirt ("7", "Barcelona Titular", "22/23", 9999, "Liga Española", "allProducts", "img/barcelona-shirt.png");
 const shirt8 = new Shirt ("8", "Atletico Madrid", "21/22", 7999, "Liga Española", "allProducts", "img/atletico-shirt.jpg");
 const shirt9 = new Shirt ("9","Porto Titular", "22/23", 7999, "Liga Portuguesa", "allProducts", "img/porto-shirt.png");
 const shirt10 = new Shirt ("10", "Benfica Titular", "22/23", 7999, "Liga Portuguesa","allProducts", "img/benfica-shirt.png");
-const shirt11 = new Shirt ("11", "Borussia Dortmund", "22/23", 9499, "Liga Alemana", "allProducts", "img/bvb-shirt.png");
+const shirt11 = new Shirt ("11", "Borussia Dortmund", "22/23", 8999, "Liga Alemana", "allProducts", "img/bvb-shirt.png");
 const shirt12 = new Shirt ("12", "Bayern Munich", "22/23", 9999, "Liga Alemana", "allProducts", "img/bayern-shirt.png");
 
 
