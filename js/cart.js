1d1e0ddd03e81d4d1af5c76811e6a52c9d33ee8a
Franco Topatigh <topatighfranco@gmail.com>
2022-07-24 18:49:12 -0300

    agregamo btn si el cart esta vacio

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,7 @@
-let basket = JSON.parse(localStorage.getItem("shirtsInCart") || [])
+let label = document.getElementById("label")
+let shoppingCart = document.getElementById("shopping-cart")
 
-console.log(basket)
+let basket = JSON.parse(localStorage.getItem("shirtsInCart")) || []
 
 // MUESTRA LA CANTIDAD DE PRODUCTOS EN EL CARRITO
 function onLoadCartNumbers(){
@@ -12,3 +13,19 @@ function onLoadCartNumbers(){
 }
 
 onLoadCartNumbers();
+
+let generateCartItems = () => {
+    if (basket.length != 0){
+        console.log("El carrito NO esta vacio")
+    } else {
+        shoppingCart.innerHTML = ``;
+        label.innerHTML = `
+        <h2>El carrito está vacio</h2>
+        <a href="../index.html">
+        <button class="btnIndex">Volver al inicio</button>
+        </a>
+        `
+    }
+};
+
+generateCartItems();
